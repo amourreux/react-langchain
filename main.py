0ddac77e6d0ebac3e980ae9fdb177710f20e1c67
Emre Portakal <emre.portakal@commencis.com>
2024-08-15 13:00:59 +0300

    feat: ReAct Loop

--- main.py
+++ main.py
@@ -1,2 +1,95 @@
-from dotenv import load_dotenv
+from typing import Union, List
 
+from dotenv import load_dotenv
+from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActSingleInputOutputParser
+from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import render_text_description, Tool
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a word by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"')  # stripping awat non alphabetic characters just in case
+    return len(text)
+
+
+def find_tool_by_name(tools: List[Tool], tool_name: str)-> Tool:
+    for tool in tools:
+        if tool.name == tool_name:
+            return tool
+    raise ValueError(f"Tool with name {tool_name} not found")
+
+
+if __name__ == '__main__':
+    print("hi ReAct LangChain!")
+    tools = [get_text_length]
+
+    # https://smith.langchain.com/hub/hwchase17/react?organizationId=f07cdf11-501d-549a-950e-6a5ff3e4e547
+    template = """
+    Answer the following questions as best you can. You have access to the following tools:
+
+    {tools}
+    
+    Use the following format:
+    
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    Begin!
+    
+    Question: {input}
+    Thought:
+    """
+
+    prompt = PromptTemplate.from_template(template=template).partial(
+        tools=render_text_description(tools), tool_names=", ".join([t.name for t in tools])
+    )
+
+    llm = ChatOpenAI(temperature=0, stop=["\nObservation"])
+
+    intermediate_steps = []
+
+    agent = {
+                "input": lambda x: x["input"],
+                "agent_scratchpad": lambda x: format_log_to_str(x["agent_scratchpad"])
+            } | prompt | llm | ReActSingleInputOutputParser()
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length in characters of the text commencis?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+    print(agent_step)
+
+    if isinstance(agent_step, AgentAction):
+        toot_name = agent_step.tool
+        tool_to_use = find_tool_by_name(tools, toot_name)
+        tool_input = agent_step.tool_input
+
+        observation = tool_to_use.func(str(tool_input))
+        print(f"{observation=}")
+        intermediate_steps.append((agent_step, str(observation)))
+
+    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+        {
+            "input": "What is the length in character of the Commencis?",
+            "agent_scratchpad": intermediate_steps,
+        }
+    )
+
+    if isinstance(agent_step, AgentFinish):
+        print(agent_step.return_values)
